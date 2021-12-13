508d236e735973e10f6645e11d5b74513cdb85f3
BcnCarlos <carlos.fung@me.com>
2021-12-13 16:19:11 +0100

    joke.routes

--- routes/joke.routes.js
+++ routes/joke.routes.js
@@ -1,4 +1,5 @@
 const { JSONCookies } = require("cookie-parser");
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
@@ -66,23 +67,60 @@ router.get("/jokes", (req, res, next) => {
 
   // NEEDS TO FIND A WAY TO SORT BY USERID BUT DOES NOT WORK
 
-  const Data = new Promise((resolve, reject) => {
-    User.findById(req.query.userId)
-      .then((User) => {
-        const favorites = new Promise((resolve, reject) => {
-          Joke.find({
-            _id: {
-              $in: User.favorites,
-            },
-          }).then((res) => resolve(res));
-        });
-        console.log(2);
-        console.log(favorites);
-        favorites.then((response) => resolve(response));
-      })
-      .catch((err) => res.json(err));
-  });
-  Data.then((res2) => res.json(res2));
+// const Data = new Promise((resolve, reject) => {
+//     User.findById(req.query.userId)
+//       .then((User) => {
+//         const favorites = new Promise((resolve, reject) => {
+//           Joke.find({
+//             _id: {
+//               $in: User.favorites,
+//             },
+//           }).then((res) => resolve(res));
+//         });
+//         console.log(2);
+//         console.log(favorites);
+//         favorites.then((response) => resolve(response));
+//       })  
+//       .catch((err) => res.json(err));
+//   });
+//   Data.then((res2) => res.json(res2));
+
+
+User.findById(req.query.userId).populate("favorites")
+.then( user => {
+res.json(user.favorites)
+}) 
+
+
 });
+
+
+//  DELETE /api/jokes/:jokeId  - Deletes a specific joke by id
+
+
+router.delete("/jokes/:id", (req, res, next) => {
+  
+
+  const { id } = req.params;
+
+  // if (!mongoose.Types.ObjectId.isValid(jokeId)) {
+  //   res.status(400).json({ message: "Specified id is not valid" });
+  //   return;
+  console.log("joke from delete", id)
+ // console.log( "req", req.query.userId)
+
+//  console.log( User.findOneAndDelete( {favorites: [jokeId] } )   )
+
+
+  Joke.findByIdAndRemove(id)
+    .then(() =>
+    
+    res.json({ message: `Joke with ${id} is removed successfully.` })
+
+      )
+    .catch((error) => res.json(error));
+});
+
+
 
 module.exports = router;
