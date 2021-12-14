41bd93910f632bee6a9c8b7dc7eb34f18eb1fa49
BcnCarlos <carlos.fung@me.com>
2021-12-14 12:12:52 +0100

    routes

---
+++ routes/product.routes.js
@@ -0,0 +1,19 @@
+const { JSONCookies } = require("cookie-parser");
+const { response } = require("express");
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Joke = require("../models/Joke.model");
+const User = require("../models/User.model");
+const Product = require("../models/Product.model")
+
+
+
+
+
+
+
+
+
+module.exports = router;
