434d7136819b18ef3876a00edb5ae218c7dc87db
BcnCarlos <carlos.fung@me.com>
2021-12-15 12:17:11 +0100

    productroutes

--- models/Product.model.js
+++ models/Product.model.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const productSchema = new Schema({
-  type: [{ type: String, required: true }],
+  type: { type: String },
   img: { type: String },
-  joke: [{ type: Schema.Types.ObjectId, ref: "Joke" }],
+  joke: { type: String },
   user: { type: Schema.Types.ObjectId, ref: "User" },
   size: { type: String },
-  color: { type: String },
 });
 
 module.exports = model("Product", productSchema);
